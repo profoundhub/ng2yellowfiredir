97d9661f1a28e015fef51509bca8136465d85d8c
Daniel Lim <hbt@markhamboards.com>
2016-07-22 17:44:13 -0400

    more changes

--- angular-cli-build.js
+++ angular-cli-build.js
@@ -16,7 +16,7 @@ module.exports = function(defaults) {
       'reflect-metadata/**/*.+(ts|js|js.map)',
       'rxjs/**/*.+(js|js.map)',
       '@angular/**/*.+(js|js.map)',
-      'angularfire2/**/*.js ',
+      'angularfire2/**/*.js',
       'firebase/lib/*.js'
     ]
   });
